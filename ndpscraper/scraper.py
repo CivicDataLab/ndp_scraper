5c3b54d724ae3cff351d6870253abd00cfd7f31f
Manjunath <hmanjub@gmail.com>
2022-07-21 16:55:58 +0530

    Scraper as on 21st July.. scraping looks fine with wait conditions

--- ndpscraper/scraper.py
+++ ndpscraper/scraper.py
@@ -1,4 +1,6 @@
+import logging
 import math
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -9,22 +11,41 @@ import re
 from variables import *
 import urllib.parse
 from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+def wait_until_loading(driver_instance: WebDriver, xpath: str, delay = 3):
+    WebDriverWait(driver_instance, delay).until(
+        EC.presence_of_element_located((By.XPATH, xpath)))
 
-def calculate_num_of_pages(driver: WebDriver):
-    count_info = driver.find_element(By.XPATH, "//div[@class='search_num_result d-block float-left']").text
+def calculate_num_of_pages(driver_instance: WebDriver):
+    wait_until_loading(driver_instance, "//div[@class='search_num_result d-block float-left']")
+    count_info = driver_instance.find_element(By.XPATH, "//div[@class='search_num_result d-block float-left']").text
     total_count = int(re.findall('\d+', "".join(count_info.split("of")[1].split()))[0])
     print(total_count)
-    records_in_page = len(driver.find_elements(By.XPATH, "//div[@class = 'card']"))
+    records_in_page = len(driver_instance.find_elements(By.XPATH, "//div[@class = 'card']"))
     print(records_in_page)
     num_of_pages = math.ceil(total_count / records_in_page)
     return num_of_pages
 
+
 def get_nids(driver: WebDriver):
     pass
 
-def get_resource_details(driver: WebDriver):
-    pass
+
+def get_resource_details(driver_instance: WebDriver):
+    wait_until_loading(driver_instance, "//a[@class='btn-preview']")
+    buttons = driver_instance.find_elements(By.XPATH, "//a[@class='btn-preview']")
+    heading_list = []
+    for i in buttons:
+        driver_instance.execute_script("arguments[0].click();", i)
+        wait_until_loading(driver_instance, "//h5[@class = 'modal-title']")
+        heading_list.append(driver_instance.find_element(By.XPATH, "//h5[@class = 'modal-title']").text)
+        wait_until_loading(driver_instance, "//h5[@class = 'modal-title']")
+        close_button = driver_instance.find_element(By.XPATH, "//button[@class = 'close']")
+        driver_instance.execute_script("arguments[0].click();", close_button)
+        time.sleep(2)
+    return heading_list
 
 
 options = Options()
@@ -33,7 +54,7 @@ options.add_argument('disable-infobars')
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
 options.add_argument('--disable-gpu')
-
+options.add_argument("headless")
 path_to_chrome_driver = "E:/chromedriver"
 driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
 
@@ -42,68 +63,77 @@ site_url = "https://data.gov.in/catalogs?"
 driver.get("https://data.gov.in/catalogs?page=1")
 
 detail_nid_tuple = ()
-for page in range(2, 4):
-    print("inside main page loop..")
-    card_link_by_xpath = driver.find_elements(By.XPATH, "//div[@class='card-header']/a")
-    # [link1, link2, link3....]
-    for i in range(0, len(card_link_by_xpath)):
-        print("traversing all the cards of the page...")
-        sub_driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
-        master_resource_url = card_link_by_xpath[i].get_attribute("href")
-        # [link1, link2, link3....] in above list
-        sub_driver.get(master_resource_url)
-        # open link1, link2 and so on...
-        try:
-            num_of_pages = calculate_num_of_pages(sub_driver)
-        except:
-            continue  # if it can't calculate any page -> no data inside that link
-        # calculate num of pages in each opened link...as first link is opened get all nids..
-        page_nid_list = sub_driver.find_elements(By.XPATH, NID_XPATH)
-        resource_detail = sub_driver.find_elements(By.XPATH, RESOURCE_DETAIL_XPATH)
-        resource_detail_list = []
-        list_of_nids = []
-        for detail_index in range(len(resource_detail)):
-            resource_detail_list.append(resource_detail[detail_index].text)
-        resource_detail_list.remove("")# TODO On examining the page, it is noticed that the resource detail will be found from the second element in the list
-        # First element is always empty
-        for nid in range(len(page_nid_list)):
-            list_of_nids.append(page_nid_list[nid].get_attribute("nid"))  # once calculated, get nids by
-            # visiting all the other pages...
-        print("CHECKING LENGTH%%%%%", len(resource_detail_list), len(list_of_nids))
-        detail_nid_tuple = detail_nid_tuple + tuple(zip(resource_detail_list, list_of_nids))
-        for j in range(2, num_of_pages):
-            # TODO remove the following 'if' after test
-            if num_of_pages > 3:
-                break
-            print("going to the next page under same resource....")
-            sub_sub_driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
-            master_resource_params = {'page': j}
-            master_resource_page_to_get = master_resource_url + "?" + urllib.parse.urlencode(master_resource_params)
-            sub_sub_driver.get(master_resource_page_to_get)  # open next page
-            page_nid_list = sub_sub_driver.find_elements(By.XPATH, NID_XPATH)
-            for nid_index in range(len(page_nid_list)):
-                list_of_nids.append(page_nid_list[nid_index].get_attribute("nid"))
-            resource_detail = sub_sub_driver.find_elements(By.XPATH, RESOURCE_DETAIL_XPATH)
-            resource_detail_list = []
-            for detail_index in range(len(resource_detail)):
-                resource_detail_list.append(resource_detail[detail_index].text)
-            resource_detail_list.remove("")  # TODO On examining the page, it is noticed that the resource detail will be found from the second element in the list
-            # First element is always empty
+for page in range(2, 4):  # TODO change the upper limit to total num of pages while prod run
+    try:
+        print("inside main page loop..")
+        wait_until_loading(driver, "//div[@class='card-header']/a")
+        card_link_by_xpath = driver.find_elements(By.XPATH, "//div[@class='card-header']/a")
+        # [link1, link2, link3....]
+        for card_link in card_link_by_xpath:
+            print("traversing all the cards of the page...")
+            sub_driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
+            master_resource_url = card_link.get_attribute("href")
+            # [link1, link2, link3....] in above list
+            try:
+                sub_driver.get(master_resource_url)
+            except:
+                logging.info("Error Loading ", master_resource_url)
+                continue
+            #time.sleep(8)
+            # open link1, link2 and so on...
+            try:
+                num_of_pages = calculate_num_of_pages(sub_driver)
+            except:
+                logging.info("No resources under ", master_resource_url)
+                continue  # if it can't calculate any page -> no data inside that link
+            # calculate num of pages in each opened link...as first link is opened get all nids..
+            wait_until_loading(sub_driver, NID_XPATH, 5)
+            page_nid_list = sub_driver.find_elements(By.XPATH, NID_XPATH)
+            # resource_detail = sub_driver.find_elements(By.XPATH, RESOURCE_DETAIL_XPATH)
+            try:
+                resource_detail_list = get_resource_details(sub_driver)
+            except:
+                logging.info("Error getting details from the page.. ",master_resource_url," moving on to the next page..")
+                continue
+            list_of_nids = []
+            for nid in page_nid_list:
+                list_of_nids.append(nid.get_attribute("nid"))  # once calculated, get nids by
+                # visiting all the other pages...
+            print("CHECKING LENGTH%%%%%", len(resource_detail_list), len(list_of_nids))
             detail_nid_tuple = detail_nid_tuple + tuple(zip(resource_detail_list, list_of_nids))
-            sub_sub_driver.close()
+            for j in range(2, num_of_pages+1):
+                # TODO remove the following 'if' after test
+                if j > 3:
+                    break
+                print("going to the next page under same resource....")
+                sub_sub_driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
+                master_resource_params = {'page': j}
+                master_resource_page_to_get = master_resource_url + "?" + urllib.parse.urlencode(master_resource_params)
+                try:
+                    sub_sub_driver.get(master_resource_page_to_get)  # open next page
+                except:
+                    logging.info("Error loading ", master_resource_page_to_get, " continuing with the next..")
+                    continue
+                wait_until_loading(sub_sub_driver, NID_XPATH, 5)
+                page_nid_list = sub_sub_driver.find_elements(By.XPATH, NID_XPATH)
+                for nid_index in range(len(page_nid_list)):
+                    list_of_nids.append(page_nid_list[nid_index].get_attribute("nid"))
+                resource_detail_list = get_resource_details(sub_sub_driver)
+                detail_nid_tuple = detail_nid_tuple + tuple(zip(resource_detail_list, list_of_nids))
+                sub_sub_driver.close()
 
-        sub_driver.close()
-    params = {'page': page}
-    url_to_get = site_url + urllib.parse.urlencode(params)
-    print("closing driver")
-    driver.close()
-    print("opening driver..")
-    driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
-    driver.get(url_to_get)
+            sub_driver.close()
+        params = {'page': page}
+        url_to_get = site_url + urllib.parse.urlencode(params)
+        driver.close()
+        driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver)
+        driver.get(url_to_get)
+        time.sleep(2)
+    except:
+        continue
 
 print(len(detail_nid_tuple))
 print(set(detail_nid_tuple))
-
 
 """Making a request ... Headers are to be updated using selectors """
 payload = '{"name":[{"value":"zcc"}],' \
--- ndpscraper/test_hovering.py
+++ ndpscraper/test_hovering.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver import ActionChains
 from selenium.webdriver.chrome.options import Options
@@ -5,6 +7,10 @@ from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.by import By
 import requests
 import re
+
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
 from variables import *
 import urllib.parse
 from selenium.webdriver.remote.webelement import WebElement
@@ -22,11 +28,28 @@ driver = webdriver.Chrome(options=options, executable_path=path_to_chrome_driver
 
 site_url = "https://data.gov.in/catalogs?"
 
-driver.get("https://data.gov.in/catalog/coverage-anganwadis-tap-connection-under-jal-jeevan-mission-jjm")
+driver.get("https://data.gov.in/catalog/answers-data-rajya-sabha-questions-session-255")
 
-card_link_by_xpath = driver.find_elements(By.XPATH, "//div[@class='card-header']")
+WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, "//a[@class='btn-preview']")))
+buttons = driver.find_elements(By.XPATH, "//a[@class='btn-preview']")
+for i in buttons:
+    print(i.text)
+heading_list = []
+for i in buttons:
+    driver.execute_script("arguments[0].click();", i)
+    heading_list.append(driver.find_element(By.XPATH, "//h5[@class = 'modal-title']").text)
+    close_button = driver.find_element(By.XPATH, "//button[@class = 'close']")
+    driver.execute_script("arguments[0].click();", close_button)
+    time.sleep(10)
 
-a = ActionChains(driver)
-m= driver.find_element(By.XPATH, "//div[@class='card-header']/span")
-a.move_to_element(m)
-print(driver.page_source)
+#print(driver.page_source)
+#print(driver.execute_script("arguments[0].click();", button))
+print(heading_list)
+# a = ActionChains(driver)
+# m= driver.find_element(By.XPATH, "")
+# a.move_to_element(m).click()
+
+# driver.execute_script("document.getElementsByClassName('btn-preview')[0].click()")
+# print(driver.execute_script("document.getElementsByClassName('btn-preview')[0].click()"))
+# print(driver.page_source)
+#print(driver.page_source)
